3e7186adc0ccb35ee5106c5416b772308029594c
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2025-06-06 00:16:07 +0800

    cleanup: remove example apps, added LivingRoom app

---
+++ apps/Area/LivingRoom/LivingRoom.cs
@@ -0,0 +1,13 @@
+using System;
+using HomeAssistantGenerated;
+
+namespace HomeAutomation.apps.Area.LivingRoom;
+
+[NetDaemonApp]
+public class LivingRoom
+{
+    public LivingRoom(Entities entities, IHaContext ha)
+    {
+        // entities.Light.SalaLightsGroup.Toggle();
+    }
+}
