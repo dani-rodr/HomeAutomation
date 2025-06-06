b2897a037e5344fb423c110d31ba731d12face96
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2025-06-06 22:06:54 +0800

    feat: enhance CookingAutomation with idle turn-off logic for rice cooker and induction; refactor state handling for improved clarity

--- apps/Area/Kitchen/Automations/CookingAutomation.cs
+++ apps/Area/Kitchen/Automations/CookingAutomation.cs
@@ -6,13 +6,38 @@ public class CookingAutomation(Entities entities, ILogger logger) : AutomationBa
 {
     private readonly NumericSensorEntity _riceCookerPower = entities.Sensor.RiceCookerPower;
     private readonly SwitchEntity _riceCookerSwitch = entities.Switch.RiceCookerSocket1;
-    private readonly BinarySensorEntity _powerThresholdExceeded = entities.BinarySensor.SmartPlug3PowerExceedsThreshold;
+    private readonly SensorEntity _airFryerStatus = entities.Sensor.CareliSg593061393Maf05aStatusP21;
+    private readonly ButtonEntity _inductionTurnOff = entities.Button.InductionCookerPower;
+    private readonly BinarySensorEntity _cookingWattageExceedThreshold = entities
+        .BinarySensor
+        .SmartPlug3PowerExceedsThreshold;
 
     public override void StartAutomation()
     {
+        AutoTurnOffRiceCookerOnIdle(minutes: 10);
+        AutoTurnOffInductionOnIdle(minutes: 12);
+    }
+
+    private void AutoTurnOffInductionOnIdle(int minutes)
+    {
+        _cookingWattageExceedThreshold
+            .StateChangesWithCurrent()
+            .WhenStateIsForMinutes(HaEntityStates.ON, minutes)
+            .Subscribe(_ =>
+            {
+                if (_airFryerStatus.State == HaEntityStates.UNAVAILABLE)
+                {
+                    _inductionTurnOff.Press();
+                }
+            });
+    }
+
+    private void AutoTurnOffRiceCookerOnIdle(int minutes)
+    {
+        var riceCookerIdlePower = 100;
         _riceCookerPower
             .StateChangesWithCurrent()
-            .WhenStateIsForMinutes(s => s?.State < 100, 10)
+            .WhenStateIsForMinutes(s => s?.State < riceCookerIdlePower, minutes)
             .Subscribe(_ => _riceCookerSwitch.TurnOff());
     }
 }
