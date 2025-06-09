a449fc64394cff7717d7b79fc915521543b35b91
Daniel Rodriguez <danielenerrodriguez@deltek.com>
2025-06-09 14:15:26 +0800

    refactor: consolidate test project global usings and remove obsolete test files

---
+++ tests/HomeAutomation.Tests/GlobalUsings.cs
@@ -0,0 +1,15 @@
+// Global usings for test project
+global using Xunit;
+global using FluentAssertions;
+global using Moq;
+global using System;
+global using System.Collections.Generic;
+global using System.Reactive.Linq;
+global using HomeAutomation.apps.Common;
+global using HomeAutomation.apps.Helpers;
+global using Microsoft.Extensions.Logging;
+global using NetDaemon.HassModel;
+global using NetDaemon.HassModel.Entities;
+global using NetDaemon.AppModel;
+global using HomeAssistantGenerated;
+global using System.Reactive.Concurrency;
