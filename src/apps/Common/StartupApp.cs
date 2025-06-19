c827b26743fd29f3e02652ace717f59872ccc419
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2025-06-20 01:36:04 +0800

    feat: Add persistent notification creation in StartupApp for better user feedback

--- src/apps/Common/StartupApp.cs
+++ src/apps/Common/StartupApp.cs
@@ -10,5 +10,14 @@ public class StartupApp : AppBase<StartupApp>
     {
         services.BrowserMod.Notification(new() { Message = "NetDaemonApp has started", ActionText = "Dismiss" });
         logger.LogDebug("NetDaemonApp has started");
+
+        services.PersistentNotification.Create(
+            new PersistentNotificationCreateParameters
+            {
+                Title = "NetDaemon",
+                Message = "Automation Started : View [Logs](/hassio/addon/c6a2317c_netdaemon5/logs)",
+                NotificationId = "netdaemon_start",
+            }
+        );
     }
 }
