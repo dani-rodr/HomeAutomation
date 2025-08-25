ed51cbccda43f150d57060c14905a881b6be9a59
dani-rodr <95452036+dani-rodr@users.noreply.github.com>
2025-08-26 06:26:01 +0800

    Update ci-cd.yml

--- src/HomeAutomation/apps/Common/StartupApp.cs
+++ src/HomeAutomation/apps/Common/StartupApp.cs
@@ -29,7 +29,7 @@ public class StartupApp : AppBase<StartupApp>
         HomeAssistantGenerated.Services services
     )
     {
-        await Task.Delay(TimeSpan.FromSeconds(10));
+        await Task.Delay(TimeSpan.FromSeconds(15));
         services.PersistentNotification.Dismiss(
             new PersistentNotificationDismissParameters { NotificationId = "netdaemon_start" }
         );
